4f476271d754f454fa7538b2005d6969385ca256
Sam Hamm <samhamm@gmail.com>
2016-12-06 11:08:37 -0800

    middle of class 12/6

--- class-07-oop-constructors-tables/demo-constructors/app.js
+++ class-07-oop-constructors-tables/demo-constructors/app.js
@@ -17,9 +17,23 @@ var juan = { //eslint-disable-line
 
 // What if... I had a function to make these many similar objects for me? A function where I could pass in a student's **unique** characteristics/behaviors as arguments, "hardwire" the shared characteristics/behaviors, and then use that function to create the individual objects in memory?
 
+function Student(firstName, lastInitial, faveNumber) { //eslint-disable-line
+  this.course = '201d17';
+  this.firstName = firstName;
+  this.lastInitial = lastInitial;
+  this.faveNumber = faveNumber;
+  this.isCodeNinja = true;
+  this.introduction = function() {
+    console.log('My name is ' + this.firstName + ' and my favorite number is ' + this.faveNumber);
+  }
+  mahClass.push(this);
+}
+
 // And then, what if I had an array where I could store all of you?
 var mahClass = []; //eslint-disable-line
 
 // Instances
+new Student('Juan', 'V', 17);
+new Student('Cherise', 'M', 4);
 
-// Instead of 275 lines, I can use an object constructor of 11 lines plus 25 one-line instantiations of individual students, and model the entire class with 36 lines.
+// Instead of 275 lines, I can use an object constructor of 11 lines plus 28 one-line instantiations of individual students, and model the entire class with 39 lines.
--- class-07-oop-constructors-tables/solution-yesterday/app.js
+++ class-07-oop-constructors-tables/solution-yesterday/app.js
@@ -13,21 +13,40 @@ var firstAndPike = {
   totalDailyCookieSales: 0,
   calcRandomCustPerHour: function() {
     // a for loop over the length of the hours array
-    this.randomCustPerHour.push('the math to calc a random number of customers between the min and the max');
+    for (var i = 0; i < hours.length; i++) {
+      this.randomCustPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+          // console.log(this.randomCustPerHour, 'random customers per hour');
+    }
   },
   calcTotalCookiesSoldEachHour: function() {
-    // call the calcRandomCustPerHour method;
+    this.calcRandomCustPerHour();
     // a for loop over the length of the hours array
-    this.totalCookiesSoldEachHour.push('the random customers per hour multiplied by the average cookies per hour');
-    // this is also a good place to calc the daily total with some kind of += action
+    for (var i = 0; i < hours.length; i++) {
+      this.totalCookiesSoldEachHour.push(Math.ceil(this.randomCustPerHour[i] * this.avgCookiesPerCust));
+      // this is also a good place to calc the daily total with some kind of += action
+      this.totalDailyCookieSales += this.totalCookiesSoldEachHour[i];
+      // console.log(this.totalDailyCookieSales, 'running total of daily cookie sales');
+    }
+    // console.log(this.totalCookiesSoldEachHour, 'cookies sold each hour');
   },
   render: function() {
     //  call the calcTotalCookiesSoldEachHour method;
+    this.calcTotalCookiesSoldEachHour();
     // grab the id of the empty ul for this location in the document
+    var emptyUL = document.getElementById('firstandpikeUL');
     // inside of a for loop, do the three parts of adding to the document
-    // create an element, give it content, and append it
-    // don't forget that in the list element you'll need to concatenate
+    for (var i = 0; i < hours.length; i++) {
+      // 6am: 16 cookies
+      // create an element, give it content, and append it
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ': ' + this.totalCookiesSoldEachHour[i] + ' cookies';
+      emptyUL.appendChild(liEl);
+      // don't forget that in the list element you'll need to concatenate
+    }
     // and don't forget that outside of the for loop you need to create, content, and add a last list element for the daily total
+    liEl = document.createElement('li');
+    liEl.textContent = 'Total: ' + this.totalDailyCookieSales + ' cookies ';
+    emptyUL.appendChild(liEl);
   }
 };
 
