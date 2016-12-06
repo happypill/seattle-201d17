8ef6ea2e55c84af350c00d299cad7445d882bb34
Sam Hamm <samhamm@gmail.com>
2016-12-06 12:08:14 -0800

    end of class 12/6

--- class-07-oop-constructors-tables/demo-tables/app.js
+++ class-07-oop-constructors-tables/demo-tables/app.js
@@ -7,23 +7,22 @@ function Cat(name, color, tail) {
   this.color = color;
   this.tail = tail;
   allCats.push(this);
+  // console.log(this);
+  render = function() {
+    // FOR YOUR LAB!!!!
+    // in here go the guts to make an individual table row
+  }
 }
 
-var bud = new Cat('Buddy', 'Orange and White', 'VERY long') //eslint-disable-line
-var alley = new Cat('Alistair', 'Orange', 'Small'); //eslint-disable-line
-var trill = new Cat('Trillian', 'Black/Orange', 'Very small'); //eslint-disable-line
-var meowmix = new Cat('Meow Mix', 'Black', 'Medium'); //eslint-disable-line
-var toeny = new Cat('Toeny', 'Gray', 'Medium-Large'); //eslint-disable-line
+new Cat('Buddy', 'Orange and White', 'VERY long');
+new Cat('Alistair', 'Orange', 'Small');
+new Cat('Trillian', 'Black/Orange', 'Very small');
+new Cat('Meow Mix', 'Black', 'Medium');
+new Cat('Toeny', 'Gray', 'Medium-Large');
 
 // for(var i = 0; i < allCats.length; i++) {
 //   console.log(allCats[i].name);
 // }
-
-// console.log(bud);
-// console.log(alley);
-// console.log(trill);
-// console.log(meowmix);
-// console.log(toeny);
 
 /* To see what we need to do to build a HTML table with JS, let's take a look at the HTML needed to start the table and make the top two rows:
 
@@ -52,8 +51,42 @@ function catTableJS() { //eslint-disable-line
   console.log('catTable is', catTable);
 
   // We need to make the header row first
+  var trEl = document.createElement('tr');
+  // create a th element, give it content, add it to tr
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Name';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = 'Color';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = 'Tail';
+  trEl.appendChild(thEl);
+
+  catTable.appendChild(trEl);
 
   // Next, we need to make rows for the rest of the cats
   // Maybe a "for" loop that iterates through the array, making a new row for each cat on each pass?
+  for (var i = 0; i < allCats.length; i++) {
+    trEl = document.createElement('tr');
 
+    var tdEl = document.createElement('td');
+    tdEl.textContent = allCats[i].name;
+    trEl.appendChild(tdEl);
+
+    tdEl = document.createElement('td');
+    tdEl.textContent = allCats[i].color;
+    trEl.appendChild(tdEl);
+
+    tdEl = document.createElement('td');
+    tdEl.textContent = allCats[i].tail;
+    trEl.appendChild(tdEl);
+
+    catTable.appendChild(trEl);
+  }
 }
+
+// FOR YOUR LAB
+// a for loop over the array of stores that calls the render() method on each individual location
